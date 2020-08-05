18ee9b32118439d05731b8f364b86559b1a707eb
davidwinalda <davidwinalda94@gmail.com>
2020-08-05 11:53:20 +0700

    Add to repo react-api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [giphy, setGiphy] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
+
+  // useEffect(() => {
+  //   const url = 'http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=R4zqJ5ZHschneHIM3WHmkEuBdif2TkOU&limit=5';
+  //   fetch(url)
+  //   .then((resp) => resp.json())
+  //   .then(function(data) {
+  //     // Here you get the data to modify as you please
+  //     console.log(data)
+  //     })
+  //   .catch(function(error) {
+  //     // If there is any error you will catch them here
+  //     console.log(error);
+  //   }); 
+  // }, []);
+
+  useEffect(() => {
+    const url = 'http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=R4zqJ5ZHschneHIM3WHmkEuBdif2TkOU&limit=5';
+    axios.get(url)
+    .then(function (result) {
+      console.log(result.data.data)
+      setGiphy(result.data.data);
+      setLoading(false);
+    })
+    .catch(function(error) {
+      setError(true);
+      console.log(error.message)
+      setErrorMessage(error.message)
+      setLoading(false);
+    });
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Call API</h2>
+      {loading ? (
+        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+      ) : (
+          error ? (
+            <div>{errorMessage}</div>
+          ) : (
+              giphy.map((item) => (
+              <ul>
+                <li><img src={item.images.original.url} alt="Giphy" /></li>
+                <li>{item.title}</li>
+              </ul>
+            ))
+          )
+      )
+      }
     </div>
   );
-}
+};
 
 export default App;
